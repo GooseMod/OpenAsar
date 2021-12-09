7f78bc8d9a4fad4c0547320abeee699b01ff7946
Oj <oj@oojmed.com>
2021-12-09 19:27:06 +0000

    [Init] Add version logging and config loading

--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
 const log = require('./utils/log');
 global.log = log; // Make log global for easy usage everywhere
 
-log('', 'Initing...');
+const package = require('./package.json');
+
+log('Init', 'OpenAsar v' + package.version);
+
+const appSettings = require('./appSettings');
+global.oaConfig = appSettings.getSettings().get('openasar', {});
+
+log('Init', 'Loaded config', oaConfig);
 
 const bootstrap = require('./bootstrap');
 
