14f342ef2fd6e3d01de7125e4571438ad4972998
Oj <oj@oojmed.com>
2021-12-11 21:35:59 +0000

    [WindowsUtils] Self rewrite, move to utils

--- src/updater/squirrelUpdate.js
+++ src/updater/squirrelUpdate.js
@@ -18,7 +18,7 @@ var _path = _interopRequireDefault(require("path"));
 
 var autoStart = _interopRequireWildcard(require("../autoStart"));
 
-var windowsUtils = _interopRequireWildcard(require("./windowsUtils"));
+var windowsUtils = _interopRequireWildcard(require("../utils/windowsUtils"));
 
 function _getRequireWildcardCache(nodeInterop) { if (typeof WeakMap !== "function") return null; var cacheBabelInterop = new WeakMap(); var cacheNodeInterop = new WeakMap(); return (_getRequireWildcardCache = function (nodeInterop) { return nodeInterop ? cacheNodeInterop : cacheBabelInterop; })(nodeInterop); }
 
---
+++ src/utils/windowsUtils.js
@@ -0,0 +1,48 @@
+const child_process = require('child_process');
+const { join } = require('path');
+
+const regExePath = process.env.SystemRoot ? join(process.env.SystemRoot, 'System32', 'reg.exe') : 'reg.exe';
+
+const spawn = (cmd, args, callback = (() => {})) => {
+  const stdout = '';
+  let process;
+
+  try {
+    process = child_process.spawn(cmd, args);
+  } catch (e) {
+    callback(e, stdout);
+  }
+
+  process.stdout.on('data', data => {
+    stdout += data;
+  });
+
+  proocess.on('error', err => { callback(err, stdout); });
+
+  process.on('exit', (code, signal) => {
+    let err = null;
+    if (code !== 0) {
+      err = new Error('Command failed: ' + (signal || code));
+
+      err.code = err.code || code;
+      err.stdout = err.stdout || stdout;
+    }
+
+    callback(err, stdout);
+  });
+};
+
+const spawnReg = (args, callback) => spawn(regExePath, args, callback);
+
+const addToRegistry = (queue, callback) => {
+  if (queue.length === 0) {
+    return callback && callback();
+  }
+
+  const args = queue.shift();
+  args.unshift('add');
+  args.push('/f');
+  return spawnReg(args, () => addToRegistry(queue, callback));
+};
+
+module.exports = { spawn, spawnReg, addToRegistry };
