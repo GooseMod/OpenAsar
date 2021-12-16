b0c0e46065bb0a5c9fc78f1d5769542726170f14
Oj <oj@oojmed.com>
2021-12-16 12:58:14 +0000

    [AutoStart] Rewrite to use switch for index require, generic stub

--- src/autoStart/index.js
+++ src/autoStart/index.js
@@ -1,3 +1,13 @@
-// Stub for now at least, also retaining OpenAsar
-
-module.exports = require('./' + process.platform);
+// Depends on platform
+switch (process.platform) {
+  case 'win32':
+    module.exports = require('./win32.js');
+    break;
+  
+  case 'linux':
+    module.exports = require('./linux.js');
+    break;
+  
+  default:
+    module.exports = require('./stub.js');
+}
--- src/autoStart/stub.js
+++ src/autoStart/stub.js
@@ -1,5 +1,5 @@
-// Stub in normal Discord
+// Stub (Darwin)
 exports.install = (callback) => { callback(); };
 exports.update = (callback) => { callback(); };
 exports.uninstall = (callback) => { callback(); };
-exports.isInstalled = (callback) => { callback(false); }; // Stub to true or false?
+exports.isInstalled = (callback) => { callback(false); };
