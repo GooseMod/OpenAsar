5ad7eabea59d03867acda14d5b45053216835462
Oj <oj@oojmed.com>
2021-12-16 12:57:57 +0000

    [FirstRun] Rewrite to use switch for index require, generic stub

--- src/firstRun/index.js
+++ src/firstRun/index.js
@@ -1,2 +1,9 @@
-// Stub for now at least
-module.exports = require('./' + process.platform);
+// Depends on platform
+switch (process.platform) {
+  case 'win32':
+    module.exports = require('./win32.js');
+    break;
+
+  default:
+    module.exports = require('./stub.js');
+}
