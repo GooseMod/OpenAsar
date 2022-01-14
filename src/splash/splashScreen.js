bb6454da3135568688815ded65504c900527fc59
Oj <oj@oojmed.com>
2022-01-14 18:14:49 +0000

    [Splash] Fix setting module path twice for new updater quickstart

--- src/splash/splashScreen.js
+++ src/splash/splashScreen.js
@@ -346,12 +346,6 @@ function initSplash(startMinimized = false) {
 
     if (newUpdater != null) { // Manually load desktop_core module path for faster requiring
       require('../utils/u2LoadModulePath')('discord_desktop_core');
-      const NodeModule = require('module');
-
-      const modulesDir = _path.default.join(paths.getExeDir(), 'modules');
-      const moduleCoreDir = _fs.default.readdirSync(modulesDir).find((x) => x.startsWith('discord_desktop_core-')); // Find desktop core dir by name
-
-      NodeModule.globalPaths.push(_path.default.join(modulesDir, moduleCoreDir)); // Add to globalPaths for requiring
     }
 
     /* if (newUpdater != null) {
