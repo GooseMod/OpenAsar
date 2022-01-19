c51e4c180e61c3c15c286e1bfd14fba60d86c99c
Oj <oj@oojmed.com>
2022-01-19 18:54:25 +0000

    [Scripts > Test] Fix using wrong InjectPoly script

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 echo "Packing asar..."
-./scripts/injectPolyfills.js
+./scripts/injectPolyfills.sh
 asar pack src app.asar # Package asar
 # asar list app.asar # List asar for debugging / testing
 
