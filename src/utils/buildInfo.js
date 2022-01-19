9bb1946e2ae37de5dd9b533d9d3ce719e5eb74ac
Oj <oj@oojmed.com>
2022-01-19 17:12:54 +0000

    [BuildInfo] Simplify code (no vars)

--- src/utils/buildInfo.js
+++ src/utils/buildInfo.js
@@ -2,7 +2,4 @@
 const { readFileSync } = require('fs');
 const { join } = require('path');
 
-const buildInfoPath = join(process.resourcesPath, 'build_info.json');
-const buildInfo = JSON.parse(readFileSync(buildInfoPath, 'utf8'));
-
-module.exports = buildInfo;
+module.exports = JSON.parse(readFileSync(join(process.resourcesPath, 'build_info.json'), 'utf8'));
