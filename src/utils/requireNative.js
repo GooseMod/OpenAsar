5c1b2a6bad02789a81c1a2296212d5fd0de95558
Kyza <kyza@kyza.net>
2022-01-21 02:48:49 -0500

    Make requireNative work on other platforms

--- src/utils/requireNative.js
+++ src/utils/requireNative.js
@@ -1,18 +1,10 @@
 // From Discord to only require native modules like discord_desktop_core
 const path = require("path");
-const fs = require("fs");
-const paths = require("../paths");
+const Module = require("module");
 
 module.paths = [];
 module.exports = (name) => {
-	const modulesPath = path.join(paths.getExeDir(), "modules");
-	const folders = fs
-		.readdirSync(modulesPath)
-		.filter((f) => fs.statSync(path.join(modulesPath, f)).isDirectory());
-	for (const folder of folders) {
-		if (folder.startsWith(name)) {
-			return require(path.join(modulesPath, folder, name));
-		}
-	}
+	const mod = Module.globalPaths.find((p) => p.includes(name));
+	if (mod) return require(path.join(mod, name));
 	throw `Module ${name} not found.`;
 };
