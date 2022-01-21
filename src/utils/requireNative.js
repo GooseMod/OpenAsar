b2aa107c00c104f034ea5b9ac11b46585961338b
Kyza <kyza@kyza.net>
2022-01-21 01:09:20 -0500

    Fix Discord on newer versions of Electron

--- src/utils/requireNative.js
+++ src/utils/requireNative.js
@@ -1,3 +1,18 @@
 // From Discord to only require native modules like discord_desktop_core
+const path = require("path");
+const fs = require("fs");
+const paths = require("../paths");
+
 module.paths = [];
-module.exports = require;
+module.exports = (name) => {
+	const modulesPath = path.join(paths.getExeDir(), "modules");
+	const folders = fs
+		.readdirSync(modulesPath)
+		.filter((f) => fs.statSync(path.join(modulesPath, f)).isDirectory());
+	for (const folder of folders) {
+		if (folder.startsWith(name)) {
+			return require(path.join(modulesPath, folder, name));
+		}
+	}
+	throw `Module ${name} not found.`;
+};
