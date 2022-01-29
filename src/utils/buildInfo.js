91f0784b00542c1cf39330d296608780cebefd95
Oj <oj@oojmed.com>
2022-01-29 11:15:32 +0000

    [BuildInfo] Tweak source

--- src/utils/buildInfo.js
+++ src/utils/buildInfo.js
@@ -1,5 +1,5 @@
-// Discord uses require but we'll use JSON parse for vaguely more security-ish
-const { readFileSync } = require('fs');
+// Use JSON parse for vaguely more security-ish
+const fs = require('fs');
 const { join } = require('path');
 
-module.exports = JSON.parse(readFileSync(join(process.resourcesPath, 'build_info.json'), 'utf8'));
+module.exports = JSON.parse(fs.readFileSync(join(process.resourcesPath, 'build_info.json'), 'utf8'));
