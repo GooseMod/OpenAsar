7fe33d3412aeb73ed2402dc2e4306cfc86ee63ee
Oj <oj@oojmed.com>
2022-01-29 11:04:49 +0000

    [AutoStart > Stub] Simplify

--- src/autoStart/stub.js
+++ src/autoStart/stub.js
@@ -1,5 +1,5 @@
 // Stub (Darwin)
-exports.install = (callback) => { callback(); };
-exports.update = (callback) => { callback(); };
-exports.uninstall = (callback) => { callback(); };
-exports.isInstalled = (callback) => { callback(false); };
+exports.install = (callback) => callback();
+exports.update = (callback) => callback();
+exports.uninstall = (callback) => callback();
+exports.isInstalled = (callback) => callback(false);
