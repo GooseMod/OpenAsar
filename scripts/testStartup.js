a82b1adb02e6470196d140326ad52e82a2f33e29
Oj <oj@oojmed.com>
2022-02-01 21:43:44 +0000

    [CI > Test] Log stderr and stdout in script

--- scripts/testStartup.js
+++ scripts/testStartup.js
@@ -1,15 +1,20 @@
 const { execFile } = require('child_process');
 
 const test = () => {
-  const proc = execFile(process.argv[2], ['--enable-logging']);
+  const proc = execFile('xvfb-run', ['-e', '/dev/stdout', process.argv[2], '--enable-logging']);
 
   let success = false;
   proc.stderr.on('data', (data) => {
+    console.error(data.toString());
     if (data.toString().includes('FAST CONNECT')) { // Main window JS running, startup success
       success = true;
       proc.kill();
     }
   });
+
+  data.stdout.on('data', (data) => {
+    console.log(data.toString();)
+  })
 
   proc.on('close', async () => {
     process.exit(success ? 0 : 1);
