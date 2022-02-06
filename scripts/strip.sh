1929cb04272e0f271b42ca9cc58ce08507aa6c5b
Oj <oj@oojmed.com>
2022-02-06 17:50:08 +0000

    [Scripts > Strip] Add removal of blank lines, add comments

--- scripts/strip.sh
+++ scripts/strip.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
-find src -type f -name "*.js" -exec sed -i -E 's@[[:blank:]]*([^:]//).*@@;T;/./!d' {} +
+find src -type f -name "*.js" -exec sed -i -E 's@[[:blank:]]*([^:]//).*@@;T;/./!d' {} + # Remove // comments (ignore URLs)
+find src -type f -name "*.js" -exec sed -i '/^[[:space:]]*$/d' {} + # Remove whitespace
