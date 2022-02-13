4955f5f8cf370816ea266656e3398954306ee182
Oj <oj@oojmed.com>
2022-02-13 21:30:05 +0000

    [Index] Simplify system electron and overlay source

--- src/index.js
+++ src/index.js
@@ -4,15 +4,8 @@ global.oaVersion = 'nightly';
 
 log('Init', 'OpenAsar', oaVersion);
 
-log('Init', 'Resources Path:', process.resourcesPath);
-if (process.resourcesPath.startsWith('/usr/lib/electron')) { // Using system electron, fix process.resourcesPath
-  log('Init', 'Detected System Electron, fixing paths');
-  global.systemElectron = true;
-
-  process.resourcesPath = require('path').join(__dirname, '..');
-
-  log('Init', 'Resources Path:', process.resourcesPath);
-}
+if (process.resourcesPath.startsWith('/usr/lib/electron')) global.systemElectron = true; // Using system electron, flag for other places
+process.resourcesPath = require('path').join(__dirname, '..'); // Force resourcesPath for system electron
 
 const appSettings = require('./appSettings');
 global.oaConfig = appSettings.getSettings().get('openasar', {});
@@ -21,16 +14,9 @@ log('Init', 'Loaded config', oaConfig);
 
 require('./cmdSwitches')();
 
-if (process.argv.includes('--overlay-host')) {
-  const buildInfo = require('./utils/buildInfo');
-
-  if (buildInfo.newUpdater) {
-    require('./utils/u2LoadModulePath')();
-  } else {
-    require('./updater/moduleUpdater').initPathsOnly(buildInfo);
-  }
-
-  require('discord_overlay2/standalone_host.js');
+if (process.argv.includes('--overlay-host')) { // If overlay
+  require('./utils/u2LoadModulePath')(); // Manually load updater 2 module paths (all modules)
+  require('discord_overlay2/standalone_host.js'); // Start overlay
 } else {
   require('./bootstrap')(); // Start bootstrap
 }
