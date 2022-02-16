d4e14ae35a781383bac40537c753ff110978c324
Oj <oj@oojmed.com>
2022-02-16 15:48:29 +0000

    [Updater > App] Move some requires inline

--- src/updater/appUpdater.js
+++ src/updater/appUpdater.js
@@ -1,13 +1,9 @@
-const moduleUpdater = require('./moduleUpdater');
 const updater = require('./updater');
 
-const settings = require('../appSettings').getSettings();
 const buildInfo = require('../utils/buildInfo');
 const Constants = require('../Constants');
 
 const { fatal, handled } = require('../errorHandler');
-const firstRun = require('../firstRun');
-const autoStart = require('../autoStart');
 const splash = require('../splash');
 
 
@@ -15,14 +11,14 @@ exports.update = (startMin, done, show) => {
   if (updater.tryInitUpdater(buildInfo, Constants.NEW_UPDATE_ENDPOINT)) {
     const inst = updater.getUpdater();
 
-    inst.on('host-updated', () => autoStart.update(() => {}));
+    inst.on('host-updated', () => require('../autoStart').update(() => {}));
     inst.on('unhandled-exception', fatal);
     inst.on(updater.INCONSISTENT_INSTALLER_STATE_ERROR, fatal);
     inst.on('update-error', handled);
 
-    firstRun.performFirstRunTasks(inst);
+    require('../firstRun').performFirstRunTasks(inst);
   } else {
-    moduleUpdater.init(Constants.UPDATE_ENDPOINT, settings, buildInfo);
+    require('./moduleUpdater').init(Constants.UPDATE_ENDPOINT, require('../appSettings').getSettings(), buildInfo);
   }
 
   splash.initSplash(startMin);
