95e62f9d624fef7d3df19380637c80d5f2489638
Oj <oj@oojmed.com>
2022-03-11 14:18:25 +0000

    [Splash > Preload] Minor context bridge tweaks / cleanup

--- src/splash/preload.js
+++ src/splash/preload.js
@@ -1,6 +1,6 @@
 const { contextBridge, ipcRenderer } = require('electron');
 
 
-contextBridge.exposeInMainWorld('DiscordSplash', {
-  onStateUpdate: callback => ipcRenderer.on('SPLASH_STATE', (_, state) => callback(state))
+contextBridge.exposeInMainWorld('Splash', {
+  onState: callback => ipcRenderer.on('SPLASH_STATE', (_, state) => callback(state))
 });
