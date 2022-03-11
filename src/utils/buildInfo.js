c829a9135b9119721c9a7d2582c2799d456bacbb
Oj <oj@oojmed.com>
2022-03-11 08:52:18 +0000

    [BuildInfo] Inline require

--- src/utils/buildInfo.js
+++ src/utils/buildInfo.js
@@ -1,5 +1,2 @@
 // Use JSON parse for vaguely more security-ish
-const fs = require('fs');
-const { join } = require('path');
-
-module.exports = JSON.parse(fs.readFileSync(join(process.resourcesPath, 'build_info.json'), 'utf8'));
+module.exports = JSON.parse(require('fs').readFileSync(require('path').join(process.resourcesPath, 'build_info.json'), 'utf8'));
