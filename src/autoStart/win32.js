b067853371ae156a3d3417c1c225748940cf75d1
Oj <oj@oojmed.com>
2022-04-05 14:13:31 +0100

    [AutoStart > win32] Minor source cleanup

--- src/autoStart/win32.js
+++ src/autoStart/win32.js
@@ -3,16 +3,14 @@ const { join, basename, dirname } = require('path');
 const registry = require('../utils/registry');
 
 const appName = basename(process.execPath, '.exe');
-const fullExeName = basename(process.execPath);
-const updatePath = join(dirname(process.execPath), '..', 'Update.exe');
 
 const queuePrefix = [ 'HKCU\\Software\\Microsoft\\Windows\\CurrentVersion\\Run', '/v', appName ];
 
 exports.install = (callback) => {
   log('AutoStart', 'Install');
 
-  const execPath = `${updatePath} --processStart ${fullExeName}` + (settings.get('START_MINIMIZED', false) ? ' --process-start-args --start-minimized' : ''); // Add Electron args if start minimized on
-  registry.add([[...queuePrefix, '/d', execPath]], callback); // Make reg
+  const execPath = join(dirname(process.execPath), '..', 'Update.exe') + ` --processStart ${basename(process.execPath)}` + (settings.get('START_MINIMIZED', false) ? ' --process-start-args --start-minimized' : ''); // Add Electron args if start minimized on
+  registry.add([[ ...queuePrefix, '/d', execPath ]], callback); // Make reg
 };
 
 exports.update = (callback) => {
@@ -24,7 +22,7 @@ exports.update = (callback) => {
 exports.uninstall = (callback) => {
   log('AutoStart', 'Uninstall');
 
-  registry.spawn(['delete', ...queuePrefix, '/f'], (_error, _stdout) => callback()); // Delete reg
+  registry.spawn([ 'delete', ...queuePrefix, '/f' ], (_error, _stdout) => callback()); // Delete reg
 };
 
-exports.isInstalled = (callback) => registry.spawn(['query', ...queuePrefix], (_error, stdout) => callback(stdout.includes(appName))); // Check reg
+exports.isInstalled = (callback) => registry.spawn([ 'query', ...queuePrefix ], (_error, stdout) => callback(stdout.includes(appName))); // Check reg
