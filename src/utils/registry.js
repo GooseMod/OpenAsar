70482376ad16acf07a9f0b3f6abd5c9a546257bc
Oj <oj@oojmed.com>
2022-04-27 21:31:36 +0100

    [Registry] Rewrite spawn

--- src/utils/registry.js
+++ src/utils/registry.js
@@ -1,15 +1,7 @@
 const CP = require('child_process');
 
 
-exports.spawn = (args, cb) => {
-  const process = CP.spawn('reg.exe', args);
-  let out = '';
-
-  process.stdout.on('data', data => out += data);
-
-  process.on('error', e => cb(e, out));
-  process.on('exit', (c, s) => cb(c !== 0 ? (s ?? c) : null, out));
-};
+exports.spawn = (args, cb) => CP.execFile('reg.exe', args, cb);
 
 exports.add = (todo, cb) => {
   const x = todo.shift();
