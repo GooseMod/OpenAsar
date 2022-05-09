c9c3b5ecff67d66af3b77ccb404a9a76cef90bc5
Oj <oj@oojmed.com>
2022-05-09 08:06:41 +0100

    [MainWindow] Don't ThemeSync if default light theme

--- src/mainWindow.js
+++ src/mainWindow.js
@@ -3,7 +3,7 @@ const themesync = async () => {
   const getVar = (name, el = document.body) => el && (getComputedStyle(el).getPropertyValue(name) || getVar(name, el.parentElement))?.trim();
 
   const bgPrimary = getVar('--background-primary');
-  if (!bgPrimary || bgPrimary === '#36393f' || bgPrimary === lastBgPrimary) return; // Default primary bg or same as last
+  if (!bgPrimary || bgPrimary === '#36393f' || bgPrimary === '#fff' || bgPrimary === lastBgPrimary) return; // Default primary bg or same as last
   lastBgPrimary = bgPrimary;
 
   const vars = [ '--background-primary', '--background-secondary', '--brand-experiment', '--header-primary', '--text-muted' ];
