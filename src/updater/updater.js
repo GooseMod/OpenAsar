44e8d8d92d6354dcec3eacf71e7ea1dd7042aa49
CanadaHonk <oj@oojmed.com>
2022-08-15 01:42:18 +0100

    mupdater: rewrite a lot to focus on Mu instead of DV2

--- src/updater/updater.js
+++ src/updater/updater.js
@@ -26,64 +26,53 @@ const getInstalled = async (useCache = true) => (useCache && _installed) || (_in
   return acc;
 }, {}));
 
-let _manifest;
-/* const getManifest = async (useCache = true) => (useCache && _manifest) || (_manifest = await new Promise(fin => https.get(`${endpoint}distributions/app/manifests/latest?platform=${platform}&channel=${releaseChannel}&arch=x86`, async res => {
-  let data = '';
-
-  res.on('data', d => data += d.toString());
-
-  res.on('end', () => fin(JSON.parse(data)));
-}))); */
-
 const MU_ENDPOINT = 'https://mu.openasar.dev';
 
-const getManifest = async (useCache = true) => (useCache && _manifest) || (_manifest = await new Promise(fin => https.get(`${MU_ENDPOINT}/${platform}/${releaseChannel}/modules.json`, async res => {
-  let data = '';
+let _manifest;
+let lastManifest;
+const getManifest = async () => {
+  if (_manifest && lastManifest > Date.now() - 1000 * 60 * 5) return _manifest; // cache for 5m
 
-  res.on('data', d => data += d.toString());
+  return await new Promise(fin => https.get(`${MU_ENDPOINT}/${platform}/${releaseChannel}/modules.json`, async res => {
+    let data = '';
 
-  res.on('end', () => {
-    const mods = JSON.parse(data);
+    res.on('data', d => data += d.toString());
 
-    fin({
-      modules: Object.keys(mods).reduce((acc, x) => {
-        acc[x] = {
-          full: {
-            module_version: mods[x],
-            url: `${MU_ENDPOINT}/${platform}/${releaseChannel}/${x}`
-          }
-        };
+    res.on('end', () => {
+      const modules = JSON.parse(data);
 
-        return acc;
-      }, {}),
-      required_modules: [ 'discord_desktop_core', 'discord_utils' ]
+      _manifest = {
+        modules,
+        required_modules: [ 'discord_desktop_core', 'discord_utils' ]
+      };
+
+      fin(_manifest);
+
+      lastManifest = Date.now();
     });
-    // fin(JSON.parse(data))
-  });
-})));
+  }));
+};
 
-const installModule = async (moduleName, _progressCallback = () => {}, force = false) => { // install module
-  log('Updater', `Installing module ${moduleName}...`);
+const installModule = async (name, _progressCallback = () => {}, force = false) => { // install module
+  log('Updater', `Installing module ${name}...`);
   const start = Date.now();
 
+  const installed = await getInstalled();
   const manifest = await getManifest();
 
-  const remote = manifest.modules[moduleName].full;
-  const version = remote.module_version;
+  const version = manifest.modules[name];
 
-  const installed = await getInstalled();
-
-  if (!force && installed[moduleName] === version) {
-    log('Updater', 'Aborting install of', moduleName, '- already installed!');
+  if (!force && installed[name] === version) {
+    log('Updater', 'Aborting install of', name, '- already installed!');
     return;
   }
 
-  log('Updater', `Downloading ${moduleName}@${version}...`);
+  log('Updater', `Downloading ${name}@${version}...`);
 
-  const path = `${moduleName}-${version}`;
+  const path = `${name}-${version}`;
 
   const tarPath = join(pendingPath, path + '.tar');
-  const finalPath = join(modulesPath, path, moduleName);
+  const finalPath = join(modulesPath, path, name);
 
   // await fs.promises.mkdir(dirname(tarPath)).catch(_ => {});
 
@@ -94,15 +83,15 @@ const installModule = async (moduleName, _progressCallback = () => {}, force = f
     state: percent === 100 ? 'Complete' : type,
     task: {
       ['Module' + type]: {
-        package_sha256: moduleName,
-        version: { module: { name: moduleName } }
+        package_sha256: name,
+        version: { module: { name } }
       }
     },
     percent
   });
 
   let downloadTotal = 0, downloadCurrent = 0;
-  https.get(remote.url, res => {
+  https.get(`${MU_ENDPOINT}/${platform}/${releaseChannel}/${name}`, res => {
     res.pipe(stream);
 
     downloadTotal = parseInt(res.headers['content-length'] ?? 1, 10);
@@ -118,7 +107,7 @@ const installModule = async (moduleName, _progressCallback = () => {}, force = f
 
   progressCallback('Download', 100);
 
-  log('Updater', `Downloaded ${moduleName}@${version}`);
+  log('Updater', `Downloaded ${name}@${version} (${(downloadTotal / 1024 / 1024).toFixed(2)} MB)`);
 
   let extractTotal = 0, extractCurrent = 0;
 
@@ -140,7 +129,7 @@ const installModule = async (moduleName, _progressCallback = () => {}, force = f
 
   progressCallback('Install', 100);
 
-  log('Updater', `Installed ${moduleName}@${version}`);
+  log('Updater', `Installed ${name}@${version}`);
   log('Updater', `Took ${(Date.now() - start).toFixed(2)}ms`);
 
   // fs.rm(tarPath, () => {}); // clean up downloaded tar after
@@ -165,7 +154,7 @@ const updateToLatestWithOptions = async (options, callback) => {
   if (lastCheck > Date.now() - 5000) return; // don't check again if already checked in the last 5s
 
   let installed = await getInstalled();
-  const manifest = await getManifest(false);
+  const manifest = await getManifest();
 
   const wanted = Object.keys(installed).concat(manifest.required_modules).filter((x, i, arr) => i === arr.indexOf(x)); // installed + required
 
@@ -175,11 +164,10 @@ const updateToLatestWithOptions = async (options, callback) => {
   const installs = [];
   for (const m of wanted) {
     const local = installed[m] ?? -1;
-    const remote = manifest.modules[m]?.full;
+    const remote = manifest.modules[m];
 
-    const remoteVer = remote?.module_version;
-    if (remoteVer && remoteVer > local) {
-      log('Updater', 'Module update:', m, local, '->', remoteVer);
+    if (remote > local) {
+      log('Updater', 'Module update:', m, local, '->', remote);
       installs.push(installModule(m, callback));
     }
   }
@@ -219,6 +207,9 @@ module.exports = {
 
     startCurrentVersion,
     collectGarbage: () => {},
+
+    getKnownFolder: _ => '',
+    createShortcut: _ => {},
 
     valid: true,
     on: events.on
