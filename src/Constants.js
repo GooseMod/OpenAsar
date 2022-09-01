eba2611ae815f824398cf496d3803ef2dad86fa7
CanadaHonk <oj@oojmed.com>
2022-09-02 00:27:03 +0100

    constants: minor clean up

--- src/Constants.js
+++ src/Constants.js
@@ -1,5 +1,4 @@
 const p = 'Discord'; // Product name
-const d = 'https://discord.com'; // Domain
 
 const r = releaseChannel; // Release channel
 const n = p + (r === 'stable' ? '' : (r[0].toUpperCase() + r.slice(1))); // Name as Discord<Channel> (if not stable)
@@ -8,5 +7,5 @@ const n = p + (r === 'stable' ? '' : (r[0].toUpperCase() + r.slice(1))); // Name
 module.exports = {
   APP_NAME: n,
   APP_ID: [ 'com', 'squirrel', n, n ].join('.'),
-  API_ENDPOINT: settings.get('API_ENDPOINT') ?? (d + '/api')
+  API_ENDPOINT: settings.get('API_ENDPOINT') ?? 'https://discord.com/api'
 };
