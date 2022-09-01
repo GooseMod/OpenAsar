3f2e863b93d4d2fda612f05795e9804b8f658c18
CanadaHonk <oj@oojmed.com>
2022-09-02 00:41:16 +0100

    autoStart > win32: tweak source

--- src/autoStart/win32.js
+++ src/autoStart/win32.js
@@ -3,13 +3,13 @@ const { join, basename } = require('path');
 const reg = (a, c) => require('child_process').execFile('reg.exe', a, c);
 
 const appName = basename(process.execPath, '.exe');
-const queuePrefix = [ 'HKCU\\Software\\Microsoft\\Windows\\CurrentVersion\\Run', '/v', appName ];
+const prefix = [ 'HKCU\\Software\\Microsoft\\Windows\\CurrentVersion\\Run', '/v', appName ];
 
 
-exports.install = cb => reg([ 'add', ...queuePrefix, '/d', join(process.execPath, '..', '..', 'Update.exe') + ' --processStart ' + basename(process.execPath) + (settings.get('START_MINIMIZED') ? ' --process-start-args --start-minimized' : ''), '/f' ], cb); // Make reg (with Electron args if start min)
+exports.install = cb => reg([ 'add', ...prefix, '/d', join(process.execPath, '..', '..', 'Update.exe') + ' --processStart ' + basename(process.execPath) + (settings.get('START_MINIMIZED') ? ' --process-start-args --start-minimized' : ''), '/f' ], cb); // Make reg (with Electron args if start min)
 
-exports.update = cb => exports.isInstalled(installed => installed ? exports.install(cb) : cb()); // Reinstall if installed, else just cb
+exports.update = cb => exports.isInstalled(inst => inst ? exports.install(cb) : cb()); // Reinstall if installed, else just cb
 
-exports.uninstall = cb => reg([ 'delete', ...queuePrefix, '/f' ], () => cb()); // Delete reg
+exports.uninstall = cb => reg([ 'delete', ...prefix, '/f' ], () => cb()); // Delete reg
 
-exports.isInstalled = cb => reg([ 'query', ...queuePrefix ], (e, out) => cb(out.includes(appName))); // Check reg
+exports.isInstalled = cb => reg([ 'query', ...prefix ], (e, out) => cb(out.includes(appName))); // Check reg
