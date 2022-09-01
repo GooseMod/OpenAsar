032967377ed989f21722b81fa80130ce256773a8
CanadaHonk <oj@oojmed.com>
2022-09-01 23:20:01 +0100

    autostart > win32: source tweaks

--- src/autoStart/win32.js
+++ src/autoStart/win32.js
@@ -6,10 +6,10 @@ const appName = basename(process.execPath, '.exe');
 const queuePrefix = [ 'HKCU\\Software\\Microsoft\\Windows\\CurrentVersion\\Run', '/v', appName ];
 
 
-exports.install = (cb) => reg([ 'add', ...queuePrefix, '/d', join(process.execPath, '..', '..', 'Update.exe') + ' --processStart ' + basename(process.execPath) + (settings.get('START_MINIMIZED') ? ' --process-start-args --start-minimized' : ''), '/f' ], cb); // Make reg (with Electron args if start min)
+exports.install = cb => reg([ 'add', ...queuePrefix, '/d', join(process.execPath, '..', '..', 'Update.exe') + ' --processStart ' + basename(process.execPath) + (settings.get('START_MINIMIZED') ? ' --process-start-args --start-minimized' : ''), '/f' ], cb); // Make reg (with Electron args if start min)
 
-exports.update = (cb) => exports.isInstalled(installed => installed ? exports.install(cb) : cb()); // Reinstall if installed, else just cb
+exports.update = cb => exports.isInstalled(installed => installed ? exports.install(cb) : cb()); // Reinstall if installed, else just cb
 
-exports.uninstall = (cb) => reg([ 'delete', ...queuePrefix, '/f' ], () => cb()); // Delete reg
+exports.uninstall = cb => reg([ 'delete', ...queuePrefix, '/f' ], () => cb()); // Delete reg
 
-exports.isInstalled = (cb) => reg([ 'query', ...queuePrefix ], (e, out) => cb(out.includes(appName))); // Check reg
+exports.isInstalled = cb => reg([ 'query', ...queuePrefix ], (e, out) => cb(out.includes(appName))); // Check reg
