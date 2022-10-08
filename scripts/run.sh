fe37d1bc3e9bf938c56ac41530055848ecb73ddc
kofi2718 <102371764+kofi2718@users.noreply.github.com>
2022-10-08 08:04:04 +0200

    fix run.sh

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,12 +1,19 @@
-#!/bin/sh
+#/bin/bash
 
+# To compile the ../src use npm install in ../src directory
+
+if [ -z "$1" ]; then
+	printf "discord-canaty app.asar directory must be suplied as the first argument!\nuse find / -name app.asar 2>/dev/null to locate it\nthe path should look like that:\n\t/opt/discord-canary/resources/app.asar\n\t/usr/share/discord-canary/resources/app.asar"
+	exit 1
+fi
 echo "Packing asar..."
-./scripts/injectPolyfills.sh
-asar pack src app.asar # Package asar
+./injectPolyfills.sh
+npx asar pack src app.asar # Package asar
 # asar list app.asar # List asar for debugging / testing
 
 echo "Copying asar..."
-cp app.asar /opt/discord-canary/resources/app.asar # Overwrite app.asar for Linux Canary
+#old #cp app.asar /opt/discord-canary/resources/app.asar # Overwrite app.asar for Linux Canary
+cp app.asar $1 || exit 1
 
 echo "Running discord..."
 echo ""
