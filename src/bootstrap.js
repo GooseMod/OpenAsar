6328f3c293670103103cdedfef42e128bb19a2aa
CanadaHonk <oj@oojmed.com>
2022-10-17 22:57:06 +0100

    ui: add channel prefix if non-nightly

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -40,7 +40,7 @@ const startCore = () => {
       const [ channel, hash ] = oaVersion.split('-'); // Split via -
 
       bw.webContents.executeJavaScript(readFileSync(join(__dirname, 'mainWindow.js'), 'utf8')
-        .replaceAll('<hash>', hash || channel)
+        .replaceAll('<hash>', channel === 'nightly' ? hash : (channel + ' ' + hash))
         .replaceAll('<notrack>', oaConfig.noTrack)
         .replace('<css>', (oaConfig.css ?? '').replaceAll('\\', '\\\\').replaceAll('`', '\\`')));
 
