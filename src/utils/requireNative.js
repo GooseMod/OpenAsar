f2da613f2b803ad97b43be5a7803f510619e578e
CanadaHonk <oj@oojmed.com>
2022-12-01 21:19:42 +0000

    requireNative: fix reversing

--- src/utils/requireNative.js
+++ src/utils/requireNative.js
@@ -2,5 +2,5 @@
 // For Updater v2: get direct path in globalPaths (g[0])
 // For Module Updater: get root path for all modules in globalPaths
 
-const g = require('module').globalPaths;
-module.exports = (n, e = '') => require(require('path').join(g.reverse().find(x => x.includes(n)) ?? g[0], n, e));
+const g = require('module').globalPaths.reverse();
+module.exports = (n, e = '') => require(require('path').join(g.find(x => x.includes(n)) ?? g[0], n, e));
