be076a2fe45d22adf90002006b2b7f20c4f1c31d
CanadaHonk <oj@oojmed.com>
2023-03-23 21:35:07 +0000

    ci: add size diff to release notes

---
+++ scripts/nightlyNotes.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+echo "$(git rev-parse HEAD | cut -c 1-7) | $(git log -1 --pretty=%B)"
+
+size_old=$(stat -c "%s" old.asar)
+size_new=$(stat -c "%s" app.asar)
+
+echo "print(\"%+2.3f KB\" % (($size_new-$size_old)/1000))" | python3
