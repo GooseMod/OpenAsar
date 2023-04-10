20b5fa3165ca5c83629075a059a050e83130eb89
CanadaHonk <oj@oojmed.com>
2023-04-10 21:19:22 +0100

    ci: disable size diff (for now)

--- scripts/nightlyNotes.sh
+++ scripts/nightlyNotes.sh
@@ -5,4 +5,4 @@ echo "$(git rev-parse HEAD | cut -c 1-7) | $(git log -1 --pretty=%B)"
 size_old=$(stat -c "%s" old.asar)
 size_new=$(stat -c "%s" app.asar)
 
-echo "print(\"%+2.3f KB\" % (($size_new-$size_old)/1024))" | python3
+# echo "print(\"%+2.3f KB\" % (($size_new-$size_old)/1024))" | python3
