593b236711edc348c8640aabe6f68eeebcaccff2
CanadaHonk <oj@oojmed.com>
2023-04-27 23:04:19 +0100

    cmdSwitches: cleanup source

--- src/cmdSwitches.js
+++ src/cmdSwitches.js
@@ -9,8 +9,7 @@ const presets = {
 
 module.exports = () => {
   let c = {};
-  const customFlags = oaConfig.customFlags ? oaConfig.customFlags.split(' ') : [];
-  for (const x of ('base,' + (oaConfig.cmdPreset || 'perf')).split(',').reduce((a, x) => a.concat(presets[x]?.split(' ')), customFlags)) {
+  for (const x of ('base,' + (oaConfig.cmdPreset || 'perf')).split(',').reduce((a, x) => a.concat(presets[x]?.split(' '))).concat((oaConfig.customFlags ?? '').split(' '))) {
     if (!x) continue;
     const [ k, v ] = x.split('=');
 
