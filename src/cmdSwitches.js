c31c8a2e6dfa61ad0fb0137f219ee64a342039b6
jake <C92i747A@pm.me>
2024-06-10 10:22:44 +0100

    Added more switches

--- src/cmdSwitches.js
+++ src/cmdSwitches.js
@@ -2,8 +2,8 @@ const { app } = require('electron');
 
 const presets = {
   'base': '--autoplay-policy=no-user-gesture-required --disable-features=WinRetrieveSuggestionsOnlyOnDemand,HardwareMediaKeyHandling,MediaSessionService', // Base Discord
-  'perf': `--enable-gpu-rasterization --enable-zero-copy --ignore-gpu-blocklist --enable-hardware-overlays=single-fullscreen,single-on-top,underlay --enable-features=EnableDrDc,CanvasOopRasterization,BackForwardCache:TimeToLiveInBackForwardCacheInSeconds/300/should_ignore_blocklists/true/enable_same_site/true,ThrottleDisplayNoneAndVisibilityHiddenCrossOriginIframes,UseSkiaRenderer,WebAssemblyLazyCompilation --disable-low-end-device-mode --v8-cache-options=code --disable-features=Vulkan --force_high_performance_gpu`, // Performance
-  'battery': '--enable-features=TurnOffStreamingMediaCachingOnBattery --enable-low-end-device-mode --force_low_power_gpu' // Known to have better battery life for Chromium?
+  'perf': `--enable-gpu-rasterization --enable-zero-copy --ignore-gpu-blocklist --enable-hardware-overlays=single-fullscreen,single-on-top,underlay --enable-features=EnableDrDc,CanvasOopRasterization,BackForwardCache:TimeToLiveInBackForwardCacheInSeconds/300/should_ignore_blocklists/true/enable_same_site/true,ThrottleDisplayNoneAndVisibilityHiddenCrossOriginIframes,UseSkiaRenderer,WebAssemblyLazyCompilation --enable-experimental-webassembly-features --disable-low-end-device-mode --v8-cache-options=code --disable-features=Vulkan --force_high_performance_gpu --disable-v8-idle-tasks`, // Performance
+  'battery': '--enable-features=TurnOffStreamingMediaCachingOnBattery --enable-low-end-device-mode --force_low_power_gpu --disable-renderer-backgrounding --disable-smooth-scrolling' // Known to have better battery life for Chromium?
 };
 
 
