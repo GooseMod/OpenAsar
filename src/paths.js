a75870297df907d43ac6565385b0af4a781cf6fe
Oliver Medhurst <honk@goose.icu>
2026-03-04 20:25:48 +0000

    paths: add new assetCachePath
    should fix linux openh264 issues (#229)

--- src/paths.js
+++ src/paths.js
@@ -1,20 +1,18 @@
 const { join, dirname, basename } = require('path');
-const fs = require('fs');
 const { app } = require('electron');
 
 const buildInfo = require('./utils/buildInfo');
 
-let userData, userDataVersioned, resourcesPath, moduleData, exeDir, installPath, logPath;
-
+let userData, userDataVersioned, resourcesPath, moduleData, exeDir, installPath, logPath, assetCachePath;
 exports.getUserData = () => userData;
 exports.getUserDataVersioned = () => userDataVersioned;
-
 exports.getResources = () => resourcesPath;
 exports.getModuleDataPath = () => moduleData;
 exports.getInstallPath = () => installPath;
 exports.getLogPath = () => logPath;
-
 exports.getExeDir = () => exeDir;
+exports.getAssetCachePath = () => assetCachePath;
+exports.cleanOldVersions = () => {}; // stub for now
 
 
 exports.init = () => {
@@ -28,8 +26,11 @@ exports.init = () => {
   moduleData = buildInfo.newUpdater ? join(userData, 'module_data') : join(userDataVersioned, 'modules');
   resourcesPath = join(process.resourcesPath);
   logPath = join(userData, 'logs');
+  assetCachePath = join(userData, 'discord_asset_cache');
 
   global.moduleDataPath = moduleData; // Global because discord
   global.logPath = logPath; // Global because discord
+  global.assetCachePath = assetCachePath;
+
   app.setPath('userData', userData); // Set userData properly because electron
 };
