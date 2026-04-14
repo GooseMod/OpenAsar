7390a9cff2927d8be8c75480834d2dcdefe29b37
Oliver Medhurst <honk@goose.icu>
2026-04-15 00:19:25 +0100

    scripts: do not use canary for run.sh

--- scripts/run.sh
+++ scripts/run.sh
@@ -5,9 +5,9 @@ asar pack src app.asar # Package asar
 # asar list app.asar # List asar for debugging / testing
 
 echo "Copying asar..."
-cp app.asar /opt/discord-canary/resources/app.asar # Overwrite app.asar for Linux Canary
+cp app.asar /opt/discord/resources/app.asar # Overwrite app.asar for Linux Canary
 
 echo "Running discord..."
 echo ""
 
-discord-canary "$@" # Run it
+discord "$@" # Run it
